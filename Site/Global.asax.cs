ad9d66788421b3acb8736598ea08b2f32f7ef2b3
Эльдар Исмагилов <eldarismagilov@gmail.com>
2014-01-27 10:12:58 +0600

    Creating Windsor container and replacing Controller Factory

--- Site/Global.asax.cs
+++ Site/Global.asax.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Site.Infrastructure.Plumbing;
 
 namespace Site
 {
@@ -12,6 +11,8 @@ namespace Site
 
 	public class MvcApplication : System.Web.HttpApplication
 	{
+		private static IWindsorContainer _container;
+
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
 			filters.Add(new HandleErrorAttribute());
@@ -35,6 +36,16 @@ namespace Site
 
 			RegisterGlobalFilters(GlobalFilters.Filters);
 			RegisterRoutes(RouteTable.Routes);
+
+			BootstrapContainer();
+		}
+
+		private static void BootstrapContainer()
+		{
+			_container = new WindsorContainer()
+				.Install(FromAssembly.This());
+			var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+			ControllerBuilder.Current.SetControllerFactory(controllerFactory);
 		}
 	}
 }
